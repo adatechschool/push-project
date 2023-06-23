708503333ef342f3eedbfefe2ab7d873e57ec0e9
quentinkerzerho <quentin.kerzerho@gmail.com>
2023-06-23 10:52:37 +0200

    J'ai modifié un peu le css, j'ai rajouté des commentaires pour expliquer

--- README.md
+++
@@ -1,4 +0,0 @@
-# mehdi-bradley-quentin
-Notre projet Anti-to-do List
-
-Project Stop Smoking started - 09/06/2023
--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 //Store authentification variables
-function getValue () {
-    var userName = document.getElementById("name").value;
-    var limit = document.getElementById("nbOfSticks").value;
-    document.getElementById("userName").innerText = "Welcome back " + userName;
-    document.getElementById("limit").innerText += " "+ limit;
-    document.getElementById("counter").innerText = `Running Counter : ${counter}`;
+function getValue() {
+  var userName = document.getElementById("name").value;
+  var limit = document.getElementById("nbOfSticks").value;
+  document.getElementById("userName").innerText = "Welcome back " + userName;
+  document.getElementById("limit").innerText += " " + limit;
+  document.getElementById("counter").innerText = `Counter : ${counter}`;
 }
 
-
-//Declaration of the main variables and initialization 
+//Declaration of the main variables and initialization
 let userName;
 let limit;
 let counter = 0;
@@ -18,26 +17,26 @@ let dataBase = [];
 
 //Event listener to increase by 1 the counter when the user click on the button "counterBtn" and update the database
 const counterButton = document.getElementById("counterBtn");
-counterButton.addEventListener('click', event => {
-    counter++;
-    dataBase.push(new Date());
-    document.getElementById("counter").innerText = `Running Counter : ${counter}`;
-    });
+counterButton.addEventListener("click", (event) => {
+  counter++;
+  dataBase.push(new Date());
+  document.getElementById("counter").innerText = `Counter : \n ${counter}`;
+});
 
 //Event listener to decrease by 1 the counter when the user click on the button "reductionBtn" and update the database
 const reductionButton = document.getElementById("reductionBtn");
-reductionButton.addEventListener('click', event => {
-    counter--;
-    dataBase.pop(new Date());
-    document.getElementById("counter").innerText = `Running Counter : ${counter}`;
-    });
+reductionButton.addEventListener("click", (event) => {
+  counter--;
+  dataBase.pop(new Date());
+  document.getElementById("counter").innerText = `Counter : ${counter}`;
+});
 
 //function to fill the money box if the user smoked less sticks than he used to.
 function fillMoneyBox() {
-    if (counter <= limit) {
-        saving = (limit-counter)*unitStickPrice;
-        moneyBox += Math.round(saving);
-        document.getElementById("moneyBox").innerText = moneyBox;
-    }
-    return moneyBox;
+  if (counter <= limit) {
+    saving = (limit - counter) * unitStickPrice;
+    moneyBox += Math.round(saving);
+    document.getElementById("moneyBox").innerText = moneyBox;
+  }
+  return moneyBox;
 }
