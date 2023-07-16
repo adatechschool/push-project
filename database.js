7fa30fd47b9f687bcf0ab7ed739604738b604514
MehdiGrimault <mehdi.grimault@gmail.com>
2023-07-16 22:43:39 +0200

    on peut s'identifier ou créer un compte et nos données sont conservées

--- database.js
+++ database.js
@@ -1,70 +1,38 @@
 //function to create a new user and store it in baserow
-function createAccount() {
-  userName = document.getElementById("name").value;
-  email = document.getElementById("email").value;
-  password = document.getElementById("password").value;
-  limit = document.getElementById("nbOfSticks").value;
-  fetch("https://api.baserow.io/api/database/rows/table/173457/?user_field_names=true", {
+async function createAccount() {
+  user = await fetch("https://api.baserow.io/api/database/rows/table/173457/?user_field_names=true", {
     method:"POST",
     headers: {
-      Authorization: "Token fpJSaQSJGm3J2noGoYzTXzrBn6cHAVUe",
-      "Content-Type": "application/json"
+      "Authorization": "Token x2iRlrA7czwFxbDMWj2v8wAzMhi0DLK4",
+      "Content-Type": "application/json",
+      "Accept": "application/json",
     },
     body: JSON.stringify({
-      "name": userName,
-      "email": email,
-      "password": password,
-      "nbOfSticks": limit,
-      "moneyBox" : 0
+      "name": document.getElementById("name").value,
+      "email": document.getElementById("email").value,
+      "password": document.getElementById("password").value,
+      "nbOfSticks": document.getElementById("nbOfSticks").value,
+      "moneyBox" : 0,
+      "counter" : 0
     })
   })
-  document.location.href="index.html"; 
+    .then( response => response.json())
+    localStorage.setItem("user",JSON.stringify(user));
+    document.location.href="index.html"
 }
 
 //function to log in
-function connexion() {
-  email = document.getElementById("email2").value;
-  password = document.getElementById("password2").value;
-  document.location.href="index.html"; 
-}
-
-
-//function to fill the money box if the user smoked less sticks than he used to.
-function fillMoneyBox() {
-    if (counter <= limit) {
-        saving = (limit-counter)*unitStickPrice;
-        moneyBox += Math.round(saving);
-        document.getElementById("moneyBox").innerText = moneyBox;
-        //updateMoneyBox();
-    }
-
-}
-
-
-
-function getUserData(id) {
-  return fetch(`https://api.baserow.io/api/database/rows/table/173457/${id}/?user_field_names=true`, {
-    method:"GET",
-    headers: {
-      Authorization: "Token fpJSaQSJGm3J2noGoYzTXzrBn6cHAVUe"
-    }
-  })
-    .then (response => response.json())
-    .then (body => body.name)
-};
-
-
-
-//function to update the moneyBox in baserow
-function updateMoneyBox () {
-  fetch("https://api.baserow.io/api/database/rows/table/173457/27/?user_field_names=true", {
-            method:"PATCH",
-            headers: {
-              Authorization: "Token fpJSaQSJGm3J2noGoYzTXzrBn6cHAVUe",
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-              "moneyBox": moneyBox
-            })
-  });
+async function connexion () {
+      let email = document.getElementById("connexionEmail").value;
+      let password = document.getElementById("connexionPassword").value;
+      user = await fetch(`https://api.baserow.io/api/database/rows/table/173457/?user_field_names=true&filter__field_1154427__equal=${email}&filter__field_1195631__equal=${password}`, {
+        method: "GET",
+        headers: {
+          "Authorization": "Token x2iRlrA7czwFxbDMWj2v8wAzMhi0DLK4",
+        }
+      })
+        .then(response => response.json())
+        .then(result => result.results[0])
+        localStorage.setItem("user",JSON.stringify(user));
+        document.location.href="index.html"
 }
--- script.js
+++ script.js
@@ -1,29 +1,57 @@
-//Declaration of the main variables and initialization 
-let userName;
-let email;
-let password;
-let limit;
-let counter = 0;
-const unitStickPrice = 0.58;
-let moneyBox = 0;
+window.onload = () => {
+
+      //Display user informations
+      document.getElementById("userName").innerText = "Welcome back " + JSON.parse(localStorage.getItem("user")).name;
+      document.getElementById("limit").innerText += " " + JSON.parse(localStorage.getItem("user")).nbOfSticks;
+      document.getElementById("counter").innerText = "Counter : " + JSON.parse(localStorage.getItem("user")).counter;
 
 
-//Display user informations
-  document.getElementById("userName").innerText = "Welcome back " + userName;
-  document.getElementById("limit").innerText += " " + limit;
-  document.getElementById("counter").innerText = `Counter : ${counter}`;
+      //Event listener to increase by 1 the counter when the user click on the button "counterBtn" and update the database
+      const counterButton = document.getElementById("counterBtn");
+      counterButton.addEventListener("click", async () => {
+          await fetch(`https://api.baserow.io/api/database/rows/table/173457/${JSON.parse(localStorage.getItem("user")).id}/?user_field_names=true`, {
+          method:"PATCH",
+          headers: {
+            "Authorization": "Token x2iRlrA7czwFxbDMWj2v8wAzMhi0DLK4",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            "counter" : Number(JSON.parse(localStorage.getItem("user")).counter) + 1
+          })
+        })
+        .then(response => response.json())
+        .then( result => localStorage.setItem("user",JSON.stringify(result)));
+        document.getElementById("counter").innerText = "Counter : " + JSON.parse(localStorage.getItem("user")).counter;
+      }
+      );
+
+      //Event listener to decrease by 1 the counter when the user click on the button "reductionBtn" and update the database
+      const reductionButton = document.getElementById("reductionBtn");
+      reductionButton.addEventListener("click", async () => {
+          await fetch(`https://api.baserow.io/api/database/rows/table/173457/${JSON.parse(localStorage.getItem("user")).id}/?user_field_names=true`, {
+          method:"PATCH",
+          headers: {
+            "Authorization": "Token x2iRlrA7czwFxbDMWj2v8wAzMhi0DLK4",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            "counter" : Number(JSON.parse(localStorage.getItem("user")).counter) - 1
+          })
+        })
+        .then(response => response.json())
+        .then( result => localStorage.setItem("user",JSON.stringify(result)));
+        document.getElementById("counter").innerText = "Counter : " + JSON.parse(localStorage.getItem("user")).counter;
+      }
+      );
+};
 
 
-//Event listener to increase by 1 the counter when the user click on the button "counterBtn" and update the database
-const counterButton = document.getElementById("counterBtn");
-counterButton.addEventListener("click", () => {
-  counter++;
-  document.getElementById("counter").innerText = `Counter : ${counter}`;}
-);
-
-//Event listener to decrease by 1 the counter when the user click on the button "reductionBtn" and update the database
-const reductionButton = document.getElementById("reductionBtn");
-reductionButton.addEventListener("click", () => {
-  counter--;
-  document.getElementById("counter").innerText = `Counter : ${counter}`;
-});
+ //function to fill the money box if the user smoked less sticks than he used to.
+ function fillMoneyBox() {
+  if (counter <= limit) {
+      saving = (limit-counter)*unitStickPrice;
+      moneyBox += Math.round(saving);
+      document.getElementById("moneyBox").innerText = moneyBox;
+      //updateMoneyBox();
+  }
+}
