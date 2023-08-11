3e862b26287d4d1d82c036e57729fbda55241e3a
MehdiGrimault <mehdi.grimault@gmail.com>
2023-08-11 16:37:11 +0200

    ajout navbar

---
+++ histo.js
@@ -0,0 +1,66 @@
+window.onload = async () => {
+
+    let user = JSON.parse(localStorage.getItem("user"));
+
+    let datas = await getConso(user.idUser);
+    var ctx = document.getElementById("chart").getContext('2d');
+    
+    var options = {
+        title: {
+          display: true,
+          text: "Ta conso au quotidien"
+        },
+        legend: {
+          position: 'bottom',
+        },
+          tooltips: {
+            enabled: true
+          },
+          plugins: {
+            datalabels: {
+              color: '#e5a124',
+            }
+          }
+        };
+
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: datas[0],
+        datasets: [{
+            label: 'Ma conso quotidienne',
+            data: datas[1],
+            fill: false,
+            borderColor: '#e5a124',
+            tension: 0.1
+          }]
+        },
+    });
+
+}
+
+async function getConso (userId) {
+    let response = await fetch("https://api.baserow.io/api/database/rows/table/187499/?user_field_names=true", {
+        method: "GET",
+        headers: {
+          "Authorization": "Token x2iRlrA7czwFxbDMWj2v8wAzMhi0DLK4",
+        }
+      })
+    let result = await response.json();
+    let consos = await result.results;
+    console.table(consos);
+    let consoById = consos.filter( conso => conso.idUser[0].value === userId);
+    console.table(consoById);
+    let labelForGraph = [];
+    let dataForGraph = [];
+    for (const conso of consoById) {
+        let label = conso.Date.slice(0,10);
+        let data = Number(conso.dailyConsumption);
+        labelForGraph.push(label);
+        dataForGraph.push(data);
+    }
+    console.table(labelForGraph);
+    console.table(dataForGraph);
+    return [labelForGraph,dataForGraph];
+}
+
--- reward.js
+++ reward.js
@@ -3,67 +3,13 @@ window.onload = async () => {
     let user = JSON.parse(localStorage.getItem("user"));
 
     document.getElementById("moneyBox").innerText = user.moneyBox;
-    console.log(user);
-    let datas = await getConso(user.idUser);
-    console.log(datas);
-    var ctx = document.getElementById("chart").getContext('2d');
     
-    var options = {
-        title: {
-          display: true,
-          text: "Ta conso au quotidien"
-        },
-        legend: {
-          position: 'bottom',
-        },
-          tooltips: {
-            enabled: true
-          },
-          plugins: {
-            datalabels: {
-              color: '#e5a124',
-            }
-          }
-        };
-
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-        labels: datas[0],
-        datasets: [{
-            label: 'Ma conso quotidienne',
-            data: datas[1],
-            fill: false,
-            borderColor: '#e5a124',
-            tension: 0.1
-          }]
-        },
-    });
 
 }
 
-async function getConso (userId) {
-    let response = await fetch("https://api.baserow.io/api/database/rows/table/187499/?user_field_names=true", {
-        method: "GET",
-        headers: {
-          "Authorization": "Token x2iRlrA7czwFxbDMWj2v8wAzMhi0DLK4",
-        }
-      })
-    let result = await response.json();
-    let consos = await result.results;
-    console.table(consos);
-    let consoById = consos.filter( conso => conso.idUser[0].value === userId);
-    console.table(consoById);
-    let labelForGraph = [];
-    let dataForGraph = [];
-    for (const conso of consoById) {
-        let label = conso.Date.slice(0,10);
-        let data = Number(conso.dailyConsumption);
-        labelForGraph.push(label);
-        dataForGraph.push(data);
-    }
-    console.table(labelForGraph);
-    console.table(dataForGraph);
-    return [labelForGraph,dataForGraph];
+class Gift {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
 }
-
